92764f8b56d310459d4e1294a4c8b744f9baf71b
gu-does-git <gu.does.dev@gmail.come>
2022-11-17 16:48:21 -0300

    "Getting Started" but better project v1

--- getting-started-but-better/src/components/ThemeToggle.jsx
+++ getting-started-but-better/src/components/ThemeToggle.jsx
@@ -1,10 +1,9 @@
 import { useEffect, useState } from 'preact/hooks'
 
 export default function ThemeToggle() {
-    const [theme, setTheme] = useState(localStorage.getItem('theme') ?? 'light')
+    const [theme, setTheme] = useState(localStorage.getItem('theme') ?? 'dark')
 
     useEffect(() => {
-        console.log(theme)
         if (theme === 'dark') {
             document.documentElement.classList.add('dark')
         } else {
@@ -15,10 +14,14 @@ export default function ThemeToggle() {
 
     return (
         <label className="absolute right-3 inline-flex cursor-pointer items-center">
-            <input type="checkbox" value="" className="peer sr-only" />
+            <input
+                type="checkbox"
+                value=""
+                className={theme + ' peer sr-only'}
+            />
             <div
                 onClick={() => setTheme(theme === 'light' ? 'dark' : 'light')}
-                className="peer h-6 w-11 rounded-full bg-gray-200 bg-gray-700 after:absolute after:top-[2px] after:left-[2px] after:h-5 after:w-5 after:rounded-full after:border after:border-gray-300 after:bg-white after:transition-all after:content-[''] peer-checked:bg-sky-600 peer-checked:after:translate-x-full peer-checked:after:border-white peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:border-gray-600 dark:peer-focus:ring-blue-800"
+                className="peer h-6 w-11 rounded-full bg-gray-700 after:absolute after:top-[2px] after:left-[2px] after:h-5 after:w-5 after:rounded-full after:border after:border-gray-300 after:bg-white after:transition-all after:content-[''] peer-focus:outline-none peer-focus:ring-2 peer-focus:ring-blue-300 peer-[.dark]:bg-indigo-600 peer-[.dark]:after:translate-x-full peer-[.dark]:after:border-white dark:border-gray-600 dark:peer-focus:ring-indigo-800"
             ></div>
         </label>
     )
--- getting-started-but-better/src/themeStore.js
+++
@@ -1,3 +0,0 @@
-import { atom } from 'nanostores'
-
-export const isThemeToggled = atom(false)
